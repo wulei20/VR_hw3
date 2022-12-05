70391cbb4c40c0f2205b2ae6a5f435fcdb2b1062
trrui <trr20@mails.tsinghua.edu.cn>
2022-12-05 16:28:29 +0800

    HW3 completed

--- Assets/arithmetic_script.cs
+++ Assets/arithmetic_script.cs
@@ -1,49 +1,117 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class arithmetic_script : MonoBehaviour
 {
     public string arithmetic;
+    private bool error;
     // Start is called before the first frame update
     void Start()
     {
-        arithmetic = "0";
+        arithmetic = "";
+        error = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        transform.Find("Text").GetComponent<Text>().text = error ? "Error" : arithmetic;
     }
 
-    public void Calculate()
+    public void Press(char button_val)
     {
-        int val = 0, tmp = 0, factor = 1;
-        bool error = false;
-        for (int i = 0; i < arithmetic.Length; i++)
+        error = false;
+        if (button_val == 'D') // Del
         {
-            if (arithmetic[i] >= '0' && arithmetic[i] <= '9')
+            if (arithmetic.Length > 0)
             {
-                tmp = arithmetic[i];
-                while (arithmetic[i + 1] >= '0' && arithmetic[i + 1] <= '9')
-                {
-                    i++;
-                    tmp = tmp * 10 + arithmetic[i];
-                }
-            }
-            else if (arithmetic[i] == '+')
-                val += tmp * factor;
-            else if (arithmetic[i] == '-')
-                factor = -factor;
-            else
-            {
-                Debug.Log("Calculate Error!");
-                error = true;
-                break;
+                arithmetic = arithmetic.Substring(0, arithmetic.Length - 1);
             }
         }
-        val += tmp * factor;
-        arithmetic = error ? "Error" : arithmetic + val.ToString();
+        else if (('0' <= button_val && button_val <= '9') || button_val == '+' || button_val == '-')
+        {
+            arithmetic += button_val;
+        }
+        else if (button_val == '=')
+        {
+            arithmetic += "=";
+            Calculate();
+        }
+    }
+
+    private void Calculate()
+    {
+        int number = 0, result = 0;
+        bool waiting_for_op = true;
+        char last_op = '+';
+        for (int i = 0; i < arithmetic.Length; i++)
+        {
+            var c = arithmetic[i];
+            if (waiting_for_op)
+            {
+                if (c == '+' || c == '-')
+                {
+                    if (last_op == '+')
+                    {
+                        result += number;
+                    }
+                    else
+                    {
+                        result -= number;
+                    }
+                    number = 0;
+                    last_op = c;
+                    waiting_for_op = false;
+                }
+                else if ('0' <= c && c <= '9')
+                {
+                    number = number * 10 + (c - '0');
+                }
+                else if (c == '=')
+                {
+                    if (last_op == '+')
+                    {
+                        result += number;
+                    }
+                    else if (last_op == '-')
+                    {
+                        result -= number;
+                    }
+                    if (i != arithmetic.Length - 1)
+                    {
+                        error = true;
+                        arithmetic = "";
+                        return;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                else
+                {
+                    error = true;
+                    arithmetic = "";
+                    return;
+                }
+            }
+            else
+            {
+                if ('0' <= c && c <= '9')
+                {
+                    number = c - '0';
+                    waiting_for_op = true;
+                }
+                else
+                {
+                    error = true;
+                    arithmetic = "";
+                    return;
+                }
+            }
+        }
+        arithmetic += result.ToString();
     }
 }
--- Assets/button_script.cs
+++ Assets/button_script.cs
@@ -6,29 +6,25 @@ using UnityEngine.UI;
 public class button_script : MonoBehaviour
 {
     public char button_val;
-    public GameObject display_box;
-    private bool selected;
+    public arithmetic_script QAQ;
+    public Button my_button;
+
     // Start is called before the first frame update
     void Start()
     {
         button_val = transform.Find("Text").GetComponent<Text>().text[0];
-        display_box = GameObject.Find("Display_box");
+        QAQ = GameObject.Find("Display_box").GetComponent<arithmetic_script>();
+        my_button = transform.GetComponent<Button>();
+        my_button.onClick.AddListener(OnClick);
     }
 
     // Update is called once per frame
-    void Update()
+    void Update() {}
+
+    
+    void OnClick()
     {
-        if (selected)
-        {
-            if (button_val != 'D')
-                display_box.GetComponent<arithmetic_script>().arithmetic += button_val;
-            else
-            {
-                int len = display_box.GetComponent<arithmetic_script>().arithmetic.Length;
-                display_box.GetComponent<arithmetic_script>().arithmetic = display_box.GetComponent<arithmetic_script>().arithmetic.Substring(0, len-1);
-            }
-            if (button_val == '=')
-                display_box.GetComponent<arithmetic_script>().Calculate();
-        }
+        // display_box.GetComponent<arithmetic_script>().arithmetic = Random.Range(0, 100).ToString();
+        QAQ.Press(button_val);
     }
 }
